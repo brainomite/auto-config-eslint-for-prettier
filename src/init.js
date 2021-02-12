f84bc25ae3b89fe594a05f04bcda81ec4a2e444c
Aaron Young <hi@aaronyoung.dev>
2021-02-12 09:43:23 -0500

    refactor: Refactor init.js into multiple helpers

--- src/init.js
+++ src/init.js
@@ -2,6 +2,25 @@
 const initFns = require("./initFns");
 
 /**
+ * helpers
+ */
+
+/**
+ * This will write out the new eslintrc file
+ *
+ * @param {object} eslintrcObj eslintrc to modify
+ * @param {string[]} extendsToAddArr array of extends to add to the eslintrc
+ * @param {string} eslintrcPath path for writing out the file
+ */
+function updateEslintrc(eslintrcObj, extendsToAddArr, eslintrcPath) {
+  const newEslintrcFileObj = initFns.addPrettierToConfig(
+    eslintrcObj,
+    extendsToAddArr
+  );
+  initFns.writeEslintrcFile(eslintrcPath, newEslintrcFileObj);
+}
+
+/**
  * This will exit in a nice manner
  */
 function exitGracefully() {
@@ -10,44 +29,64 @@ function exitGracefully() {
 }
 
 /**
- * main function
+ * returns the path of eslintrc if found
+ *
+ * @returns {string} found path
  */
-async function init() {
-  let eslintrcPath;
+// eslint-disable-next-line consistent-return
+function tryToGetEslintrcPath() {
   try {
-    eslintrcPath = initFns.getEslintrcPathStr();
+    return initFns.getEslintrcPathStr();
   } catch (error) {
     console.error(
       "No .eslintrc.* files located please run:\nnpx eslint --init\n"
     );
     exitGracefully();
   }
+}
 
-  const eslintrcObj = initFns.getEslintObj(eslintrcPath);
-
-  const dependenciesArr = initFns.getStrArrayOfDependencies();
-  const extendsToAddArr = initFns.getExtendsAdditionStrArr(dependenciesArr);
-
-  if (process.argv.includes("rules=thinkful")) {
-    try {
-      await initFns.addRulesToConfig(eslintrcObj);
-    } catch (error) {
-      console.error("There was a problem downloading the rules!");
-      exitGracefully();
-    }
+/** will attempt to obtain rules
+ *
+ * @param {object} eslintrcObj object to add rules to
+ */
+async function tryToAddRules(eslintrcObj) {
+  try {
+    await initFns.addRulesToConfig(eslintrcObj);
+  } catch (error) {
+    console.error("There was a problem downloading the rules!");
+    exitGracefully();
   }
+}
 
-  const newEslintrcFileObj = initFns.addPrettierToConfig(
-    eslintrcObj,
-    extendsToAddArr
-  );
-  initFns.writeEslintrcFile(eslintrcPath, newEslintrcFileObj);
-
+/**
+ * attempts to install packages
+ *
+ * @param {string} dependenciesArr string of packages to install
+ */
+async function tryInstallPrettierExtensions(dependenciesArr) {
   try {
     await initFns.installPrettierExtensions(dependenciesArr);
   } catch (error) {
     exitGracefully();
   }
+}
+
+/**
+ * main function
+ */
+async function init() {
+  const eslintrcPath = tryToGetEslintrcPath();
+  const eslintrcObj = initFns.getEslintObj(eslintrcPath);
+  const dependenciesArr = initFns.getStrArrayOfDependencies();
+  const extendsToAddArr = initFns.getExtendsAdditionStrArr(dependenciesArr);
+
+  if (process.argv.includes("rules=thinkful")) {
+    await tryToAddRules(eslintrcObj);
+  }
+
+  updateEslintrc(eslintrcObj, extendsToAddArr, eslintrcPath);
+
+  await tryInstallPrettierExtensions(dependenciesArr);
 }
 
 module.exports = { init };
